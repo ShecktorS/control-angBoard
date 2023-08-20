f4c120e41da2b2fe3b0d699a63f56176be18bc92
ShecktorS <ettoresanfilippo@gmail.com>
2023-08-20 13:16:38 +0200

    aggiunzione dati dinamici

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -10,21 +10,43 @@ export class DataService {
       name: 'Nike',
       img: 'https://picsum.photos/300/300?1',
       location: 'Milano',
-      products: [],
+      products: [
+        { title: 'scarpa', description: 'Una bella scarpa', idProduct: 1 },
+        {
+          title: 'tovaglia',
+          description: 'Una tovaglia ricamata',
+          idProduct: 2,
+        },
+        {
+          title: 'maglietta',
+          description: 'Una maglietta prodotta in Giappone',
+          idProduct: 3,
+        },
+        { title: 'tappeto', description: 'Una tappeto di lusso', idProduct: 4 },
+      ],
     },
     {
       id: 2,
       name: 'Adidas',
       img: 'https://picsum.photos/300/300?2',
       location: 'Roma',
-      products: [],
+      products: [
+        { title: 'scarpa', description: 'Una bella scarpa', idProduct: 1 },
+        {
+          title: 'tovaglia',
+          description: 'Una tovaglia ricamata',
+          idProduct: 2,
+        },
+      ],
     },
     {
       id: 3,
       name: 'Londale',
       img: 'https://picsum.photos/300/300?3',
       location: 'Palermo',
-      products: [],
+      products: [
+        { title: 'tappeto', description: 'Una tappeto di lusso', idProduct: 4 },
+      ],
     },
   ];
 
